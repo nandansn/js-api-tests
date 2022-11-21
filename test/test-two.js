9a926dae34d5be9419e44a9a2886e452b900844e
nandansn <geeknanda13@gmail.com>
2022-11-21 11:36:20 +0530

    updated

---
+++ test/test-two.js
@@ -0,0 +1,37 @@
+before('Before Suite Once', () => {
+    console.log('Before Hook')
+});
+
+after('After Suite', () => {
+    console.log("After Hook");
+});
+
+
+beforeEach('Before Test', () => {
+    console.log("Before test case");
+});
+
+afterEach('After test ', () => {
+    console.log("After test case");
+});
+
+
+describe('First Sample Test Suite -1', () => {
+    it('Test Case - 1', () => {
+        console.log('Hello Test !!!');
+    });
+
+    it("Test Case - 2", () => {
+      console.log("Hello Test !!!");
+    });
+
+    it("Test Case - 3", () => {
+      console.log("Hello Test !!!");
+    });
+
+    it("Test Case - 4", () => {
+      console.log("Hello Test !!!");
+    });
+});
+
+
